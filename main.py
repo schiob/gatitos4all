1848f96dfc73e432b666a7be8e9867449f365252
fanborges <fanborges@gmail.com>
2017-08-23 04:02:01 -0500

    Se modifica el archivo main y se agrega la clase menu

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import webbrowser
 import sys
-
+from menu import menu
 def main():
-    URL = 'http://cataas.com/cat/gif/says/'
-    webbrowser.open('{}{}'.format(URL, sys.argv[1]))
+    a = menu.eligeGato();
+    while (a!= 0): a = menu.eligeGato();	
 
 # Si el archivo es el hilo principal que se ejecute si no pos no juejuejue
 if __name__ == '__main__':
