927e5e3e32818f91521295a1d74b9e20de1cb009
CMAREQ <temomareq@gmail.com>
2017-08-25 11:00:01 -0500

    Modifica index de lista en linea 6

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 
 def main():
     URL = 'http://cataas.com/cat/gif/says/'
-    webbrowser.open('{}{}'.format(URL, sys.argv[1]))
+    webbrowser.open('{}{}'.format(URL, sys.argv[0]))
 
 # Si el archivo es el hilo principal que se ejecute si no pos no juejuejue
 if __name__ == '__main__':
