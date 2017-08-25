af440587e3c268a1bcb19623ba671c978a67d554
icarreons <akatsuki96_ivan@hotmail.com>
2017-08-25 15:04:09 -0500

    cambios sencillos para probar

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import sys
 
 def main():
     URL = 'http://cataas.com/cat/gif/says/'
-    webbrowser.open('{}{}'.format(URL, sys.argv[1]))
-
+    webbrowser.open('{}{}'.format(URL, sys.argv[2]))
+#comentario super-util
 # Si el archivo es el hilo principal que se ejecute si no pos no juejuejue
 if __name__ == '__main__':
     main() # Corre la función principal
