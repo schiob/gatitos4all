2f76813872eaca377dbe47c1625fe82d48b3f3a3
icarreons <akatsuki96_ivan@hotmail.com>
2017-08-25 15:06:54 -0500

    cambios para saber que hace cada cosa

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import sys
 
 def main():
     URL = 'http://cataas.com/cat/gif/says/'
-    webbrowser.open('{}{}'.format(URL, sys.argv[2]))
+    webbrowser.open('{}{}'.format(URL, sys.argv[1]))
 #comentario super-util
 # Si el archivo es el hilo principal que se ejecute si no pos no juejuejue
 if __name__ == '__main__':
     main() # Corre la función principal
+elif _name_ == '  ':
+    main() 
